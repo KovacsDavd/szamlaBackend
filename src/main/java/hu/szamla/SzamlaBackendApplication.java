3af8b9773535278e9918523721065e26bb71f5e2
Kovács Dávid <d.david010313@gmail.com>
2025-02-27 13:06:03 +0100

    init project

---
+++ src/main/java/hu/szamla/SzamlaBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.szamla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzamlaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzamlaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/szamla/SzamlaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.szamla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzamlaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
